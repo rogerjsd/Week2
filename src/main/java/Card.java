84bec300fc989ceeb30a4edad076d19482244103
shengdao jin <jinshen@shernet.sheridancollege.ca>
2020-01-16 15:24:08 -0500

    Card project first commit

---
+++ src/main/java/Card.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bunnie
+ */
+public class Card {
+    private int number; 
+    private static final String[] suits = {"Hearts","Diamonds","Spaces","Clubs"}; 
+    
+    
+}
